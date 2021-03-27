f92c3d1061649bbc7c1c1cb583dfcd45432d2427
RabeeOmran-dev <rabeeomran.dev@gmail.com>
2021-03-27 23:31:15 +0300

    using axios.GET to fetch data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+
+async function getUsers() {
+  try {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+    console.log(response);
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 class App extends Component{
 
+  state = {
+    users:[]
+  }
 
+  componentDidMount = ()=> {
+    getUsers().then(response => {
+      this.setState({users : response.data})
+    });
+  }
+  
   render(){
+
+
 
      
     return (
@@ -11,7 +34,12 @@ class App extends Component{
       <div>
 
 
-          <h1>TEST</h1>  
+          <ul>
+            {this.state.users.map(user => {
+                return <li>{user.name}</li>
+            })}
+           
+          </ul>
 
 
       </div>
